d420e2e24eeb86c64d6ea03d9d338e4fb76901a6
saiasok1 <saiasok1k@gmail.com>
2026-02-16 15:51:30 +0530

    updated serveStatic function

--- server/vite.ts
+++ server/vite.ts
@@ -87,7 +87,6 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
