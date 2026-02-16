ee679d96c814e3254613581036906ce4d087ddeb
saiasok1 <saiasok1k@gmail.com>
2026-02-16 21:09:54 +0530

    updated some changes

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,15 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+
+// Fix for ESM (__dirname is undefined in ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 app.use(express.json());
